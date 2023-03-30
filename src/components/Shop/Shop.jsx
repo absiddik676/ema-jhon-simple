57a6e1d41ff48ae973f8c59236c00b059376b773
absiddik676 <absiddik8200@gmail.com>
2023-03-29 20:57:21 -0700

    first commit

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Shop.css'
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                <h3>Product</h3>
+            </div>
+            <div className="cart-container">
+                <h2>oder summary</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
